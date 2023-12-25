faa0476446a5454af26b7553ae04cd7995798d47
gjt <jiangtian.g@telelands.com>
2023-12-25 16:19:02 +0800

    feat: add extra config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import preact from '@astrojs/preact'
 
-import preact from "@astrojs/preact";
+import { remarkReadingTime } from './src/scripts/remark-reading-time.mjs'
+import { remarkModifiedTime } from './src/scripts/remark-modified-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://festack-astro-template.netlify.app',
-  integrations: [vue(), tailwind(), react(), preact()]
-});
+	site: 'https://festack-astro-template.netlify.app',
+	integrations: [vue(), tailwind(), react(), preact()],
+	markdown: {
+		remarkPlugins: [remarkReadingTime, remarkModifiedTime]
+	}
+})
---
+++ src/scripts/remark-modified-time.mjs
@@ -0,0 +1,22 @@
+import { execSync } from 'child_process'
+import { statSync } from 'fs'
+
+console.log('execSyncexecSyncexecSync', execSync)
+
+// 运行一个 Git 命令获取最后一次提交的 ISO 8601 时间戳（推荐）
+export function remarkModifiedTime() {
+	return function (tree, file) {
+		const filepath = file.history[0]
+		const result = execSync(`git log -1 --pretty="format:%cI" ${filepath}`)
+		file.data.astro.frontmatter.lastModified = result.toString()
+	}
+}
+
+// 也可以使用文件系统的修改时间
+// export function remarkModifiedTime() {
+// 	return function (tree, file) {
+// 		const filepath = file.history[0]
+// 		const result = statSync(filepath)
+// 		file.data.astro.frontmatter.lastModified = result.mtime.toISOString()
+// 	}
+// }
